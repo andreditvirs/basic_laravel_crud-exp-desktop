c17ad9e83c2cb4ffad006e6a1a127a9a16c11d61
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-07-13 09:01:14 +0700

    ONG:Laravel 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,12 @@ class HomeController extends Controller
 {
     public function index(){
         $subtitle = "Sub Title";
-        return View('index', compact('subtitle'));
+        // return View('index', compact('subtitle')); // Bisa menggunakan ini
+        $data = array(
+            'subtitle' => 'Home Page',
+            'desc' => ['Web Design', 'Code', 'Developer']
+        );
+        // return View('index')->with('subtitle', $subtitle); // Bisa menggunakan ini
+        return View('index')->with($data);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>{{$subtitle}}</h1>
+    {{-- Bisa menggunakan dua cara echo ata {{...}} --}}    
+    <h1><?php echo $subtitle ?></h1>
+    @if (count($desc) > 0) {{-- Jika desc panjangnya lebih dari 0 artinya array --}}
+        <ul>
+            @foreach ($desc as $desc_item)
+                <li>{{$desc_item}}</li>
+            @endforeach
+        </ul>
+    @endif
+    {{-- <h1>{{$subtitle}}</h1> --}}
 @stop
